7501bf62e2ed19488b346684b43d1def85932d1e
jaswante6 <jaswantevaishnavi6@gmail.com>
2025-06-20 14:15:21 +0530

    first commit

--- data_entry_project/settings.py
+++ data_entry_project/settings.py
@@ -76,12 +76,16 @@ WSGI_APPLICATION = 'data_entry_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'whitefield_data_entry',
+        'USER': 'root',
+        'PASSWORD': 'vaishnavi@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        
 }
 
-
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
